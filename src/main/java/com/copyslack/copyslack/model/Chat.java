d12f61929ea094dfe15b9d1851bb4723512f5c61
peace-duke <72694799+ahlebnikov1995@users.noreply.github.com>
2023-07-22 22:25:53 +0300

    22.07.2023

--- src/main/java/com/copyslack/copyslack/model/Chat.java
+++ src/main/java/com/copyslack/copyslack/model/Chat.java
@@ -5,14 +5,34 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.List;
+
 @AllArgsConstructor
 @Builder
 @Data
 @NoArgsConstructor
+@Table(name = "chats")
+@Entity
 public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false,unique = true)
     private String name;
+
+    @Column(nullable = false,unique = true)
     private Long masterId;
+
+    @Column
     private String photoUrl;
+
+    @Column
     private String description;
+
+
+
+
+
 }
--- src/main/java/com/copyslack/copyslack/model/Message.java
+++ src/main/java/com/copyslack/copyslack/model/Message.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
 import java.util.UUID;
 
@@ -12,11 +13,26 @@ import java.util.UUID;
 @Builder
 @Data
 @NoArgsConstructor
+@Table(name = "messages")
+@Entity
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID uid;
-    private Long chatId;
-    private Long authorId;
+
+    @ManyToOne(targetEntity = Chat.class,cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH},fetch = FetchType.EAGER)
+    @JoinColumn(name = "chat_id")
+    private Chat chat;
+
+    @ManyToOne(targetEntity = User.class,cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH},fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Column(nullable = false)
     private String text;
+
+    @Column(nullable = false)
     private Date createTime;
 
 }
